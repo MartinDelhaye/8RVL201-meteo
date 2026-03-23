bef3872ca8a5c5d0bfe1398cf6e830fc3a4c0a05
Martin <marijnissennathan@gmail.com>
2026-03-23 13:13:17 -0400

    Ajout Pluie

--- Assets/Scripts/gestionFenetre.cs
+++ Assets/Scripts/gestionFenetre.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class GestionFenetre  : MonoBehaviour
 {
+    int lastCode = -1; // code météo par défaut
+    [Header("Particles")]
+    public GameObject rainParticles;
+    public GameObject snowParticles;
+    public ParticleSystem rainPS;
+    public ParticleSystem snowPS;
+
     [Header("Skyboxes")]
     public Material skySun;
     public Material skyNight;
@@ -23,46 +30,62 @@ public class GestionFenetre  : MonoBehaviour
     }
 
     IEnumerator WaitForMeteo()
-    {
-        // ⏳ attendre que l'API réponde
-        while (!RecupMeteo.Instance.isDataRecuperee)
-        {
-            yield return null;
-        }
-
+    {while (!RecupMeteo.Instance.isDataRecuperee)
+        {yield return null;}
         UpdateMeteo();
+    }
+
+    void Update()
+    {
+        if (!RecupMeteo.Instance.isDataRecuperee) return;
+        int currentCode = RecupMeteo.Instance.weatherCode;
+        if (currentCode != lastCode) // 11 est un code météo par défaut
+        {
+            lastCode = currentCode;
+            UpdateMeteo();
+        }
     }
 
     void UpdateMeteo()
     {
-        int codeMeteo = 30;
+        int codeMeteo = 11;
         bool isDay = RecupMeteo.Instance.isDay;
 
         bool isSun = (codeMeteo >= 0 && codeMeteo <= 19);
         bool isCloud = (codeMeteo >= 20 && codeMeteo <= 49);
         bool isSnow = (codeMeteo >= 70 && codeMeteo <= 79);
         bool isRain = (codeMeteo >= 50 && codeMeteo <= 69) || (codeMeteo >= 80 && codeMeteo <= 99);
+        rainParticles.SetActive(true);
+        snowParticles.SetActive(true);
 
         if (isSnow)
         {
             RenderSettings.skybox = skyRain;
 
             planeRenderer.material.SetTexture("_BaseMap", camNeige);
+            rainPS.Stop();
+            snowPS.Play();
         }
         else if (isRain)
         {
             RenderSettings.skybox = skyRain;
             planeRenderer.material.SetTexture("_BaseMap", camEte);
+            snowPS.Stop();
+            rainPS.Play();
         }
         else if (isCloud)
         {
             RenderSettings.skybox = skyCloud;
             planeRenderer.material.SetTexture("_BaseMap", camEte);
+            rainPS.Stop();
+            snowPS.Stop();
         }
         else if (isSun)
         {
             RenderSettings.skybox = skySun;
             planeRenderer.material.SetTexture("_BaseMap", camEte);
+            rainPS.Stop();
+            snowPS.Stop();
         }
 
         // 🔥 important pour la lumière
