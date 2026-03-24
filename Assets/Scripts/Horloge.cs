93f531f52505cd286a3f03fabb06cb6c4c985c14
MartinDelhaye <delhayemar1@gmail.com>
2026-03-23 22:36:51 -0400

    Gros Refacto : organisation calques suites à la présentation + réorganisation des lights + add de furnitures + add de la date à l'horloge

--- Assets/Scripts/Horloge.cs
+++ Assets/Scripts/Horloge.cs
@@ -10,7 +10,7 @@ public class Horloge : MonoBehaviour
     void Update()
     {
         DateTime currentTime = DateTime.Now;
-        string formattedTime = currentTime.ToString("HH:mm:ss");
+        string formattedTime = currentTime.ToString("dd-MM-yyyy | HH:mm:ss");
         heureText.text = formattedTime;
     }
 }
